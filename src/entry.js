627c37f27877ee42788d9a1e330fb56833450eb3
Sara Harvey <saraharveynyc@gmail.com>
2020-10-17 09:04:32 -0400

    added functions for edit and delete

--- src/entry.js
+++ src/entry.js
@@ -1,32 +1,52 @@
-// array of object instances
 class Entry {
-
-// mapping values from DB onto new entry instances:
   constructor(entry, entryAttributes) {
-    this.id = entry.id;
-    this.date = entryAttributes.date;
-    this.content = entryAttributes.content;
-    this.type = entryAttributes.type;
-    // we do a GET anyway, why not store them in an array for future use?
-    Entry.all.push(this);
+    this.id = entry.id
+    this.date = entryAttributes.date
+    this.content = entryAttributes.content
+    this.category = entryAttributes.category
+    Entry.all.push(this)
   }
-
-// put data ids on divs so we can find them again
 
   renderEntryCard() {
     return `
       <div data-id=${this.id}>
         <p>${this.date}</p>
         <p>${this.content}</p>
-        <p>${this.type.name}</p>
-        <button data-id=${this.id}>edit</button>
+        <p>${this.category.name}</p>
+        <button id="edit-button" data-id=${this.id} onclick="editEntries(this)">edit</button></p>
+        <button id="delete-button" data-id=${this.id} onclick="deleteEntries(this)">delete</button></p>
       </div>
-      <br><br>`;
+      <br><br>`
   }
+
+  renderUpdateForm() {
+      return `
+      <form data-id=${this.id} >
+        <h3>Edit an entry</h3>
+
+        <label>Date</label>
+        <input id='input-date' type="text" name="date" value="${this.date}" class="input-text">
+        <br><br>
+
+        <label>Content</label>
+        <input id='input-content' type="text" name="content" value="${this.content}" class="input-text">
+        <br><br>
+
+        <label>Category</label>
+        <select id="categories" name="categories" value="${this.category.name}">
+          <option value="1">event</option>
+          <option value="2">quote</option>
+        </select>
+        <br><br>
+
+        <input id='edit-button' type="submit" name="submit" value="Edit this entry" class="submit">
+      </form>
+    `;
+    }
+
+    static findById(id) {
+      return this.all.find(entry => entry.id === id);
+    }
 }
 
-//  static findById(id) {
-//    return this.all.find(entry => entry.id === id);
-
-// global scope, outside the class
 Entry.all = [];
--- src/index.js
+++ src/index.js
@@ -1,37 +1,56 @@
-const endPoint = "http://localhost:3000/entries";
+const endPoint = "http://localhost:3000/entries"
 
 document.addEventListener('DOMContentLoaded', () => {
    getEntries()
 
-  const createEntryForm = document.querySelector("#create-entry-form")
-
-createEntryForm.addEventListener("submit", (e) => 
-    createFormHandler(e))
-  })
+  const createSyllabusForm = document.querySelector("#create-entry-form")
+  createSyllabusForm.addEventListener("submit", (e) => createFormHandler(e))
+  const entryContainer = document.querySelector("#entries-container")
+})
 
 function getEntries() {
   fetch(endPoint)
     .then(res => res.json())
     .then(entries => {
       entries.data.forEach(entry => {
-      const newEntry = new Entry(entry.id, entry.attributes)
-
-      document.querySelector('#entries-container').innerHTML += newEntry.renderEntryCard();
-    })
+      let newEntry = new Entry(entry, entry.attributes)
+      document.querySelector('#entries-container').innerHTML += newEntry.renderEntryCard()
+      })
      // .catch(err => console.log(err))
-  })
+    })
+}
+
+function editEntries(elmnt) {
+      const id = elmnt.dataset.id;
+      const entry = Entry.findById(id);
+      document.querySelector('#update-entry').innerHTML = entry.renderUpdateForm();
+      document.querySelector('#update-entry').addEventListener('submit', e => updateFormHandler(e));
+}
+
+function deleteEntries(elmnt) {
+	const id = elmnt.dataset.id;
+	const entry = Entry.findById(id);
+
+	let delObj = { method: "DELETE" }
+	fetch(`http://localhost:3000/entries/${entry.id}`, delObj)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (json) {
+            elmnt.parentNode.remove(elmnt);           
+        })
 }
 
 function createFormHandler(e) {
   e.preventDefault()
   const dateInput = document.querySelector('#input-date').value
   const contentInput = document.querySelector('#input-content').value
-  const typeId = parseInt(document.querySelector('#types').value)
-  postFetch(dateInput, contentInput, typeId)
+  const categoryId = parseInt(document.querySelector('#categories').value)
+  postFetch(dateInput, contentInput, categoryId)
 }
 
-function postFetch(date, content, type_id) {
-  const bodyData = {date, content, type_id}
+function postFetch(date, content, category_id) {
+  const bodyData = {date, content, category_id}
 
   fetch(endPoint, {
     method: "POST",
@@ -45,4 +64,32 @@ function postFetch(date, content, type_id) {
     let newEntry = new Entry(entryData, entryData.attributes)
     document.querySelector('#entries-container').innerHTML += newEntry.renderEntryCard()
   })
+}
+
+function updateFormHandler(e) {
+  e.preventDefault();
+  //const id = parseInt(e.target.dataset.id);
+  const id = e.target.dataset.id;
+  const entry = Entry.findById(id);
+  const date = e.target.querySelector('#input-date').value;
+  const content = e.target.querySelector('#input-content').value;
+  const category_id = e.target.querySelector('#categories').value;
+  //const category_id = parseInt(e.target.querySelector('#categories').value);
+  patchEntry(entry, date, content, category_id)
+  // the new content is considered THE content at this point
+}
+
+function patchEntry(entry, date, content, category_id) {
+  const bodyJSON = { entry, date, content, category_id }
+  fetch(`http://localhost:3000/entries/${entry.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: 'application/json',
+    },
+    body: JSON.stringify(bodyJSON) //,
+  })
+    .then(response => response.json())
+    // our backend responds with the updated syllabus instance represented as JSON
+    .then(updatedEntry => console.log(updatedEntry));
 }
