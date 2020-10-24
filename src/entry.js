999b39d054fed6966c4252424e77f5a28a60658b
Sara Harvey <saraharveynyc@gmail.com>
2020-10-24 10:20:13 -0400

    Fixing style bugs

--- src/entry.js
+++ src/entry.js
@@ -10,8 +10,8 @@ class Entry {
   renderEntryCard() {
     return `
       <div data-id=${this.id}>
-        <p style="font-family: Amatic SC, sans-serif; font-size: 2em;">
-        ${this.date}&nbsp &nbsp &nbsp${this.content} — ${this.category.name}&nbsp &nbsp &nbsp
+        <p style="font-family: Amatic SC, sans-serif; font-size: 2em; display: inline;">
+        ${this.date}&nbsp &nbsp &nbsp${this.content} — ${this.category.name} &nbsp &nbsp &nbsp </p>
         
         <button id="edit-button" data-id=${this.id} onclick="editEntries(this)">
           edit</button>
@@ -26,31 +26,22 @@ class Entry {
       <form data-id=${this.id}>
         <h3>Edit an entry</h3>
 
-        <label id="date">Date</label>&nbsp &nbsp
+        <label>Date</label>&nbsp &nbsp
         <input id='input-date' type="date" name="date" value="${this.date}" class="input-text">
         <br><br>
 
-        <label id="content">Content</label>&nbsp &nbsp
+        <label>Content</label>&nbsp &nbsp
         <input id='input-content' type="text" name="content" value="${this.content}" class="input-text">
         <br><br>
 
-        <label id="category">Category</label>&nbsp &nbsp
-        <select id="input-category" name="categories" value="${this.category.name}">
+        <label>Category</label>&nbsp &nbsp
+        <select id="categories" name="categories" value="${this.category.name}">
           <option value="1">event</option>
           <option value="2">quote</option>
         </select>
         <br><br>
 
-        <input id='edit-button' type="submit" name="submit" value="Edit this entry" class="submit"
-          style="color: black;
-          font-family: Amatic SC, sans-serif;
-          font-size: 1.5em;
-          font-weight: bold;
-          background-color: #f3d899;
-          padding: 4px 32px;
-          border-radius: 10px 10px;
-          border: none;"
-        >
+        <input id='edit-button' type="submit" name="submit" value="Edit this entry" class="submit" style="background-color: #fae4b1;">
       </form>
     `;
     }
